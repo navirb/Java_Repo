6a3edda1ecc049e60a5c78244b0524917dd09427
navirb <rajvy1432@gmail.com>
2021-10-03 18:25:10 +0530

    Adding Pattern3.java

---
+++ Java_Interview_Questions/Display Patterns/Display_Patterns/src/com/navir/Pattern3.java
@@ -0,0 +1,22 @@
+package com.navir;
+/*
+ * 1 1 1 1 1 
+ * 1       1 
+ * 1       1 
+ * 1       1 
+ * 1 1 1 1 1 
+ */
+public class Pattern3 {
+	public static void main(String[] args) {
+		for (int rows = 1; rows <= 5; rows++) {
+			for (int cols = 1; cols <= 5; cols++) {
+				if (rows == 1 || rows == 5 || cols == 1 || cols == 5) {
+					System.out.print(1 + " ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
