ff9f604fc8b37fd5063912e1ef15b79e55565d3e
navirb <rajvy1432@gmail.com>
2021-10-03 13:48:25 +0530

    Commit

---
+++ Java_Interview_Questions/Display Patterns/Display_Patterns/src/com/navir/Pattern1.java
@@ -0,0 +1,17 @@
+package com.navir;
+/**
+ * 
+ * @author Nagaraju
+ *
+ */
+public class Pattern1 {
+	public static void main(String[] args) {
+		int toprint_Digits = 1;
+		for (int i = 1; i < 5; i++) {
+			for (int j = 1; j <=i; j++) {
+				System.out.print(toprint_Digits++ +" ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
